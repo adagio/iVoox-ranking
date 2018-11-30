7b6c073062cd19ec18b645efb4addebc539fab92
Franco Cedillo <franco.cedillo@gmail.com>
2018-11-30 13:26:10 -0500

    update meta description and keywords

--- src/components/layout.js
+++ src/components/layout.js
@@ -22,8 +22,8 @@ const Layout = ({ children }) => (
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
+            { name: 'description', content: 'Ranking of iVoox podcast in Tecnología category' },
+            { name: 'keywords', content: 'iVoox, tecnología, podcast' },
           ]}
         >
           <html lang="en" />
